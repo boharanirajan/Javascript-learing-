d8f2b8f351e6751889dfef3387881c7e3861d70e
Nirajan Bohara <156586279+boharanirajan@users.noreply.github.com>
2025-01-17 07:30:17 +0000

    data  type

---
+++ basic/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version 
+
+let name="nirajan";
+let age =22;
+
+let isLoggedIn=false;
+let state;
+// primitive  data types
+// numbber => 2 power 53
+// string 
+// boolean
+// null
+// undefined
+// symbol=> unique
+
+console.log(typeof(undefined));
+
