18e6ccab6c0be277601f0e2c5ec4fb36b3912861
Nirajan Bohara <156586279+boharanirajan@users.noreply.github.com>
2025-01-18 04:40:17 +0000

    operaion and conversion

---
+++ basic/functionex.js
@@ -0,0 +1,21 @@
+// let score="nirajan";
+// console.log(typeof(score));
+
+// let valueNumber=Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "33abc"=>NaN
+// ture=>1; false=>0
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+// 1=>true, 0=false
+// "" => false
+// "NIrajan"=true
+
+let someNumber=33;
+let StringNumber=String(someNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
