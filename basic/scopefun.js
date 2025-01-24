31563f614c50827bf2993d6ff8dceb72fd986eff
Nirajan Bohara <156586279+boharanirajan@users.noreply.github.com>
2025-01-24 06:25:18 +0000

    secod time

--- basic/scopefun.js
+++ basic/scopefun.js
@@ -1,3 +1,55 @@
-let a=10;
-const b=20;
-var c=30;
+// // // let a=10;
+// // // const b=20;
+// // // var c=30;
+
+
+// // function One ()
+// // {
+// //     const username="nirajan";
+
+// //     function two()
+// //     {
+// //         const wbsite="youtube";
+// //         console.log(username);
+// //     }
+// //     //console.log(website);
+// //     two();
+// // }
+// // One();
+
+
+
+// function addOne(num){
+//      return num+1;
+// }
+// //console.log(addOne(5));
+
+// const addTwo=function(num)
+// {
+//     return num+2;
+// }
+// //console.log(addTwo(5));
+
+
+
+// const user={
+//     username:"nirajan",
+//     price:999,
+//     welcomeMessage:function(){
+//         console.log(`${this.username}, welcome to website`);
+//     }
+// }
+
+// //user.welcomeMessage()
+
+
+// //\\\
+
+// //const addNum=(num1,num2)=>({username:"nirajan"})
+
+// // Immediately invoked function Expressions 
+
+(function initializeDatabaseConnection() {
+    console.log('Initializing database connection...');
+    // Add actual database connection logic  here
+})();
