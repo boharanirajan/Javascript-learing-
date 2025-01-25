764c71ce3be675cd7cf0e2ae46270cbe294a083c
Nirajan Bohara <156586279+boharanirajan@users.noreply.github.com>
2025-01-25 13:09:24 +0000

    first commit

---
+++ basic/controlflow.js
@@ -0,0 +1,60 @@
+// //
+// const month=3;
+
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("february");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("apirl");
+//         break;
+//     case 5:
+//         console.log("may ");
+//         break;
+//     case 6:
+//         console.log("june");
+//         break;
+//     case 7:
+//         console.log("july");
+//         break;
+//     case 8:
+//         console.log(" August");
+//         break;
+//         default:
+//             console.log("no match")
+//             break;
+// }
+
+// junuary , february, march, april, may june , july , August, september , october, november, december
+// sunday monday, tuesday wednesday ,thursday, friday ,saturday 
+
+
+
+// const empobj={};
+// if(Object.keys(empobj).length===0)
+// {
+//     console.log("Object is empty");
+// }
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1;
+//val1=5?? 10
+//val1=null ?? 10
+//val1=undefined ?? 15
+// val1=null ?? 10 ?? 23
+// console.log(val1);
+
+// Terniary Operator 
+
+// const price=100;
+// price<=80 ? console.log("less than 80"):console.log("More  than 80")
+
+
+
